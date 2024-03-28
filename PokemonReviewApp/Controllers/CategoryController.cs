be95c3a3f79bc2414525aa27590a00233befaed9
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-03-28 18:32:39 +0100

    chore: add line

--- PokemonReviewApp/Controllers/CategoryController.cs
+++ PokemonReviewApp/Controllers/CategoryController.cs
@@ -53,6 +53,7 @@ namespace PokemonReviewApp.Controllers
         public IActionResult GetPokemonByCategoryId(int categoryId)
         {
             var pokemons = _mapper.Map<List<PokemonDto>>(_categoryRepository.GetPokemonByCategory(categoryId));
+
             if (!ModelState.IsValid)
                 return BadRequest(ModelState);
 
