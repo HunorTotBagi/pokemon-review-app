f41bbbd14d40f26e113095735c9f4f6dab71b72c
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-03-31 11:47:21 +0200

    fix: remove internal set

--- PokemonReviewApp/Dto/OwnerDto.cs
+++ PokemonReviewApp/Dto/OwnerDto.cs
@@ -3,8 +3,8 @@
     public class OwnerDto
     {
         public int Id { get; set; }
-        public string FirstName { get; internal set; }
-        public string LastName { get; internal set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string Gym { get; set; }
     }
 }
